72d42f3526d658ba4d79a2a466abdf50b85933b1
Hai <phai86528@.gmai.com>
2026-04-10 20:59:55 +0700

    Scaffold database to generate models and DbContext

---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirstDemo;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public DateOnly? DateOfBirth { get; set; }
+
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+}
---
+++ StudentManagementContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirstDemo;
+
+public partial class StudentManagementContext : DbContext
+{
+    public StudentManagementContext()
+    {
+    }
+
+    public StudentManagementContext(DbContextOptions<StudentManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.CourseId).HasName("PK__Course__C92D71A75882CAD3");
+
+            entity.ToTable("Course");
+
+            entity.Property(e => e.CourseName).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.EnrollmentId).HasName("PK__Enrollme__7F68771B836E4DF6");
+
+            entity.ToTable("Enrollment");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.CourseId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Enrollmen__Cours__3C69FB99");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Enrollmen__Stude__3B75D760");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Student__32C52B99715F4B2E");
+
+            entity.ToTable("Student");
+
+            entity.HasIndex(e => e.Email, "UQ__Student__A9D105342D6A2A81").IsUnique();
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
